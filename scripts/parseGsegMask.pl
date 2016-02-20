64a9398393a596541d2684277fb9ede27949387a
Jon Duvick <jduvick@gmail.com>
2016-02-19 21:23:44 -0700

    fixed bug in fasta header parsing with id - description header type, which resulted in missing N-masked track

--- scripts/parseGsegMask.pl
+++ scripts/parseGsegMask.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-## Updated 6/12/14 
+## Updated 2/19/16 
 ##SYNTAX
 print("Usage: ./$0 <unmasked_file> <masked_file> <sql_output> <fasta_output> <base>\n");
 
@@ -47,87 +47,89 @@ while (<MASK>)
 
     if (@pieces=($line1 =~ /^>gi\|(\d+?)\|(.*?)\n.+/))
     {  ## regex for a GenBank fasta header with line feed, e.g. >gi|12345|gb|ABC123| This is description\n
-	##TODO: Set the gi, and other details from this.
-		$gi_line = ">gi|".$pieces[0].$pieces[1];
-		$gi = $pieces[0];
-		$desc = $pieces[1];
-		$M = 0; # first sequence, closest to header.
-		$valid="T";
-		$heading="GenBank";
-		
-#	   print "gi=$gi; gi_line=$gi_line; desc=$desc; M=$M; heading=$heading; length=$length; line1=$line1;\n";
-	}
-	elsif(@pieces=($line1 =~ /^>(\S+?)([^\S\n]+?\S+?)\n.*?/)) # [^\S\n] means not not whitespace and not newline
-	{  ## regex for a non-GenBank fasta header with description and line feed, e.g. >scaff_12345 This is description\n
-		$gi_line = ">".$pieces[0].$pieces[1];
-		$gi = $pieces[0];
-		$desc = $pieces[1];
-		$M = 0;
-		$valid="T";
-		$heading="simple with description";
-#	    print "gi=$gi;  gi_line=$gi_line; desc=$desc; M=$M; heading=$heading; line1=$line1\n";
-	}
-	elsif(@pieces=($line1 =~ /^>(\S+?)\n.*/))
-	{  ## regex for a non-GenBank fasta header with description and line feed, e.g. >scaff_12345 This is description\n
-		$gi_line = ">".$pieces[0].$pieces[1];
-		$gi = $pieces[0];
-		$desc = "";
-		$M = 0;
-		$valid="T";
-		$heading="simple with no description";
-#	   print "gi=$gi;  gi_line=$gi_line; desc=$desc; M=$M;  heading=$heading; line1=$line1\n";
-	}
-	else
-	{
-	print "Error: This unmasked fasta header cannot be read. line1=$line1\n";
-		$valid="F";
-	}
+    ##TODO: Set the gi, and other details from this.
+        $gi_line = ">gi|".$pieces[0].$pieces[1];
+        $gi = $pieces[0];
+        $desc = $pieces[1];
+        $M = 0; # first sequence, closest to header.
+        $valid="T";
+        $heading="GenBank";
+        
+#      print "gi=$gi; gi_line=$gi_line; desc=$desc; M=$M; heading=$heading; length=$length; line1=$line1;\n";
+    }
+    elsif(@pieces=($line1 =~ /^>(\S+?)\s+(.+?)\n.*?/)) #
+    {  ## regex for a non-GenBank fasta header with description and line feed, e.g. >scaff_12345 This is description\n
+    
+       ## >NW_003307545.1 Volvox carteri f. nagariensis unplaced genomic scaffold VOLCAscaffold_1, whole genome shotgun sequence
+        $gi_line = ">".$pieces[0].$pieces[1];
+        $gi = $pieces[0];
+        $desc = $pieces[1];
+        $M = 0;
+        $valid="T";
+        $heading="simple with description";
+#       print "gi=$gi;  gi_line=$gi_line; desc=$desc; M=$M; heading=$heading; line1=$line1\n";
+    }
+    elsif(@pieces=($line1 =~ /^>(\S+?)\n.*/))
+    {  ## regex for a non-GenBank fasta header with no description and line feed, e.g. >scaff_12345\n
+        $gi_line = ">".$pieces[0].$pieces[1];
+        $gi = $pieces[0];
+        $desc = "";
+        $M = 0;
+        $valid="T";
+        $heading="simple with no description";
+#      print "gi=$gi;  gi_line=$gi_line; desc=$desc; M=$M;  heading=$heading; line1=$line1\n";
+    }
+    else
+    {
+    print "Error: This unmasked fasta header cannot be read. line1=$line1\n";
+        $valid="F";
+    }
 
-	## assign line1 to the sequence (non-header) portion of the line.
-	$seq1 = substr($line1, length($gi_line) + 1, length($line1) - length($gi));
-	$seq1_stripped = $seq1;
-	$seq1_stripped =~ s/\n//g; ## strip linefeeds
+    ## assign line1 to the sequence (non-header) portion of the line.
+    $seq1 = substr($line1, length($gi_line) + 1, length($line1) - length($gi));
+    $seq1_stripped = $seq1;
+    $seq1_stripped =~ s/\n//g; ## strip linefeeds
 #    print "seq1_stripped=$seq1_stripped\n\n";
 
 
-	if($valid eq "T")
-	{
-		#Search the MASK string for sequences of X+
-		##Record the matches, and construct the SQL statements.
-		while ($seq1_stripped =~ /(${base}+)/g) 
-		{
-			$M = $M + 1;
-			print $-[0]." ".$+[0]."\n";
-			$lpos = $-[0] + 1;
-			$rpos = $+[0];
-			$subseq1=`/usr/local/bin/blastdbcmd -db $rawfile -entry $gi -range ${lpos}-${rpos} -outfmt %s`;
-			if($subseq1 =~ /(\S+)\n/)
-			{
-			  $subseq1 = $1;
-			}
-			#$seq=chomp($seq);
+    if($valid eq "T")
+    {
+        #Search the MASK string for sequences of X+
+        ##Record the matches, and construct the SQL statements.
+        while ($seq1_stripped =~ /(${base}+)/g) 
+        {
+            $M = $M + 1;
+            print $-[0]." ".$+[0]."\n";
+            $lpos = $-[0] + 1;
+            $rpos = $+[0];
+            $subseq1=`/usr/local/bin/blastdbcmd -db $rawfile -entry $gi -range ${lpos}-${rpos} -outfmt %s`;
+            if($subseq1 =~ /(\S+)\n/)
+            {
+              $subseq1 = $1;
+            }
+            #$seq=chomp($seq);
 
-			$subseq2=`/usr/local/bin/blastdbcmd -db $maskfile -entry $gi -range ${lpos}-${rpos} -outfmt %s`;
-			if($subseq2 =~ /(\S+)\n/)
-			{
-			  $subseq2 = $1;
-			}
-			if($subseq1 eq $subseq2) # was already N-string
-			{
-			  $isMask="False";
-			  $maskType="Unknown bases";
-			}
-			else
-			{
-			  $isMask="True";
-			  $maskType="Repeat Masked Region";
-			}
-			  printsql($gi, $M, $desc, $subseq1, $lpos, $rpos, $isMask, $maskType);
-#		print "gi=$gi, M=$M, desc=$desc, seq=$seq, lpos=$lpos, rpos=$rpos\n\n";
-		}
-		$start = tell MASK;  ## NEW START, from where the pointer was, includes line feeds.
-		print "NEW START=$start\n\n";
-	}
+            $subseq2=`/usr/local/bin/blastdbcmd -db $maskfile -entry $gi -range ${lpos}-${rpos} -outfmt %s`;
+            if($subseq2 =~ /(\S+)\n/)
+            {
+              $subseq2 = $1;
+            }
+            if($subseq1 eq $subseq2) # was already N-string
+            {
+              $isMask="False";
+              $maskType="Unknown bases";
+            }
+            else
+            {
+              $isMask="True";
+              $maskType="Repeat Masked Region";
+            }
+              printsql($gi, $M, $desc, $subseq1, $lpos, $rpos, $isMask, $maskType);
+#       print "gi=$gi, M=$M, desc=$desc, seq=$seq, lpos=$lpos, rpos=$rpos\n\n";
+        }
+        $start = tell MASK;  ## NEW START, from where the pointer was, includes line feeds.
+        print "NEW START=$start\n\n";
+    }
 }
 
 close MASK;
